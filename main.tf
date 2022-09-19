b2b2c18c5aa7a85e615445c9591a4ad3fd2f0e02
Grant Orchard <go@hashicorp.com>
2022-09-20 06:41:51 +1000

    module to resource

--- main.tf
+++ main.tf
@@ -40,19 +40,31 @@ data "vault_generic_secret" "this" {
 # 	subnet_id = data.terraform_remote_state.aws-core.outputs.public_subnets[0]
 # }
 
-# resource "aws_security_group" "this" {
-# 	name        = "rdp_ingress"
-#   vpc_id      = local.vpc_id
-# }
+resource "aws_security_group" "this" {
+	name        = "rdp_ingress"
+  vpc_id      = local.vpc_id
+}
 
-# resource "aws_security_group_rule" "allow_serf_udp_ingress" {
-#   protocol          = "tcp"
-#   security_group_id = aws_security_group.this.id
-# 	cidr_blocks       = ["27.32.248.192/32"]
-#   from_port         = 3389
-#   to_port           = 3389
-#   type              = "ingress"
-# }
+resource "aws_security_group_rule" "allow_serf_udp_ingress" {
+  protocol          = "tcp"
+  security_group_id = aws_security_group.this.id
+	cidr_blocks       = ["27.32.248.192/32"]
+  from_port         = 3389
+  to_port           = 3389
+  type              = "ingress"
+}
+
+resource "aws_instance" "this" {
+	ami = "ami-085cd86733cd29a21"
+	key_name = "go-rsa"
+
+	name = "ad_manager"
+	subnet_id = data.terraform_remote_state.aws-core.outputs.public_subnets[0]
+
+	security_group_id = aws_security_group.this.id
+
+	associate_public_ip_address = true
+}
 
 
 
