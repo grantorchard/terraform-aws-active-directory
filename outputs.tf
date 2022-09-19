70cb3c1824e82c4cb38a3bb2f5f2f9248ac7152a
Grant Orchard <go@hashicorp.com>
2022-09-19 22:44:18 +1000

    nuke instance

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "endpoint" {
 	value = aws_directory_service_directory.this.access_url
 }
 
-output "password" {
-	value = module.ec2-instance.password_data
-}
+# output "password" {
+# 	value = module.ec2-instance.password_data
+# }
