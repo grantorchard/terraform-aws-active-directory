4a875a2cca04b1a67186fcf65f1f8f830a8a4b51
Grant Orchard <go@hashicorp.com>
2022-09-20 12:15:57 +1000

    explicitly define egress

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ resource "aws_security_group" "egress" {
 resource "aws_security_group_rule" "permit_egress" {
   protocol          = "tcp"
   source_security_group_id = aws_security_group.egress.id
-	security_group_id = aws_security_group.egress.id
+	#security_group_id = aws_security_group.egress.id
   from_port         = 0
   to_port           = 0
   type              = "egress"
