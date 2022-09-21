22494212f4713a429150c837b79c884317bcc1ae
Grant Orchard <go@hashicorp.com>
2022-09-21 11:35:13 +1000

    add cidr_blocks

--- main.tf
+++ main.tf
@@ -64,7 +64,8 @@ resource "aws_security_group" "egress" {
 
 resource "aws_security_group_rule" "permit_egress" {
   protocol          = "-1"
-  self			= true
+  cidr_blocks       = ["0.0.0.0/0"]
+	security_group_id = aws_security_group.egress.id
   from_port         = 0
   to_port           = 0
   type              = "egress"
