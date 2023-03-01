ea9866b2a11d04e45fcbf2d44492ed9820ee5b63
Grant Orchard <go@hashicorp.com>
2023-03-01 14:05:45 +1100

    remove ec2 instance output

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "dns_servers" {
 	value = aws_directory_service_directory.this.dns_ip_addresses
 }
 
-output "windows_ip" {
-	value = aws_instance.this.public_ip
-}
+# output "windows_ip" {
+# 	value = aws_instance.this.public_ip
+# }
