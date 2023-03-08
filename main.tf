0aece7c528208f627d3e677d28d750e540c0a094
Grant Orchard <go@hashicorp.com>
2023-03-09 08:31:00 +1100

    add instance policy for domain join

--- main.tf
+++ main.tf
@@ -81,7 +81,7 @@ resource "aws_instance" "this" {
 	ami = "ami-039965e18092d85cb"
 	instance_type = "t3.small"
 	key_name = "go-rsa"
-  iam_instance_profile = aws_iam_instance_profile.this.name
+  iam_instance_profile = "domain_join"
 	tags = {
 		Name = "ad-stuff"
 	}
@@ -97,9 +97,4 @@ resource "aws_instance" "this" {
 			security_groups
 		]
 	}
-}
-
-resource "aws_iam_instance_profile" "this" {
-  name = "domain_join"
-  role = data.aws_iam_role.this.arn
 }
